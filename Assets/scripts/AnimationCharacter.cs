acb77506e383e7a97fd5f18fdebad19b6f0a9714
victormirmor <luzenmividavictor@gmail.com>
2026-05-03 13:34:24 -0300

    remove assets extras

--- Assets/scripts/AnimationCharacter.cs
+++ Assets/scripts/AnimationCharacter.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class AnimationCharacter : MonoBehaviour{
     public Animator animator;
     
-   public void Animate(bool Speed){
-        animator.SetBool("Walk", Speed);
+   //public void Animate(bool Speed){
+   //     animator.SetBool("Walk", Speed);
+   // }
+
+    public void Animate(float SX, float SZ){
+        animator.SetFloat("SpeedX",SX);
+        animator.SetFloat("SpeedZ",SZ);
+
     }
 }
--- Assets/scripts/Character_Controller.cs
+++ Assets/scripts/Character_Controller.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Character_Controller : MonoBehaviour {
     public AnimationCharacter a_Character;
+    public Transform Chica;
     [Header("Movement")]
+    public float SpeedZ;
+    public float SpeedX;
     public CharacterController controller;
     [Range(0.5f, 20)] public float walkSpeed,strafeSpeed;
     private Vector3 velocity = Vector3.zero;
@@ -11,20 +14,26 @@ public class Character_Controller : MonoBehaviour {
         Movement();
     }
     private void Movement() {
-        float _InputZ=Input.GetAxis("Vertical");
-        float _InputX=Input.GetAxis("Horizontal");
-        bool IsWalk;
+        float _InputZ=Input.GetAxisRaw("Vertical");
+        float _InputX=Input.GetAxisRaw("Horizontal");
+        //bool IsWalk;
+        Chica.transform.rotation= Quaternion.Euler(0,-90*_InputX,0);
+        SpeedZ=_InputZ;
+        SpeedX=_InputX;
+        if(SpeedZ<0.1){
+            transform.localScale=new Vector3(1,1,-1);
+            }
+        if(SpeedZ>0.1){
+            transform.localScale=new Vector3(1,1,1);
+            }
+        
+    
+
         velocity.z =_InputZ * walkSpeed;
         velocity.x =_InputX* strafeSpeed;
         velocity = transform.TransformDirection(velocity);
         velocity.y += Physics.gravity.y * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-         if (_InputX!=0 || _InputZ!=0){
-            IsWalk=true;
-        }else {
-            IsWalk=false;
-        }
-
-        a_Character.Animate( IsWalk);
+        a_Character.Animate( SpeedZ, SpeedX);
     }
 }
